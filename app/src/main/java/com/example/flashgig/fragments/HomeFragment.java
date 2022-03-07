d99e7e254bd3df2a5bce452d9a393c162d4d2f8f
kennethfaurillo <kenneth.edison.faurillo@eee.upd.edu.ph>
2022-03-08 01:12:02 +0800

    Redesigned job card Hid password text Set Single line for most text inputs Added on job click expanding text Renamed theme

--- app/src/main/java/com/example/flashgig/fragments/HomeFragment.java
+++ app/src/main/java/com/example/flashgig/fragments/HomeFragment.java
@@ -2,14 +2,17 @@ package com.example.flashgig.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Toast;
 
 import com.example.flashgig.R;
