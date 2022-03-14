1c01572593e6b145df7d4cb9c047c5fadd708cf5
OyoMagpayo <80026713+OyoMagpayo@users.noreply.github.com>
2022-03-14 15:55:40 +0800

    Job Adder Activity V2
    not yet tested

---
+++ app/src/main/java/com/example/flashgig/activities/JobAdderActivityV2.java
@@ -0,0 +1,38 @@
+package com.example.flashgig.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.flashgig.R;
+
+public class JobAdderActivityV2 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_adder_v2);
+
+        Spinner spinner =findViewById(R.id.spinner_region);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.numberOfWorkers, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
