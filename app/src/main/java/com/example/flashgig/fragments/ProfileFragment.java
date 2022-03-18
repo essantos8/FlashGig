64a19d040e470c1f4bdfa501c2cf5623cdc3118d
kennethfaurillo <kenneth.edison.faurillo@eee.upd.edu.ph>
2022-03-19 03:28:12 +0800

    Added functionality to profile/edit page

--- app/src/main/java/com/example/flashgig/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/flashgig/fragments/ProfileFragment.java
@@ -10,30 +10,42 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.signature.ObjectKey;
+import com.example.flashgig.GlideApp;
 import com.example.flashgig.R;
 import com.example.flashgig.activities.JobAdderActivity;
 import com.example.flashgig.activities.ProfileEditActivity;
 import com.example.flashgig.activities.ReviewsActivity;
 import com.example.flashgig.activities.SplashActivity;
 import com.example.flashgig.databinding.FragmentProfileBinding;
+import com.example.flashgig.models.User;
+import com.github.javafaker.Bool;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 
 public class ProfileFragment extends Fragment {
-
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+    private FirebaseUser currentUser;
 
+    private FragmentProfileBinding binding;
     private TextView textName, textEmail, textPhone;
+    private ImageView profilePicture;
+
+    private User user;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,50 +53,28 @@ public class ProfileFragment extends Fragment {
 
         db = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+        currentUser = mAuth.getCurrentUser();
     }
 
-    private void retrieveInfo(){
 
-        db.collection("users").whereEqualTo("email", mAuth.getCurrentUser().getEmail()).get().addOnCompleteListener(task -> {
-            if(task.isSuccessful()){
-                QueryDocumentSnapshot user;
-                if(!task.getResult().iterator().hasNext()){
-                    Toast.makeText(getContext(), "User not found in database!", Toast.LENGTH_SHORT).show();
-                    Log.d("TAG","user not in database");
-                    return;
-                }
-                user = task.getResult().iterator().next();
-
-                textName.setText(user.getString("fullName"));
-                textEmail.setText(user.getString("email"));
-                textPhone.setText(user.getString("phone"));
-//                Log.d("TAG",user.getId() + " => " + user.getData());
-                // for multiple results
-//                for(QueryDocumentSnapshot documentSnapshot : task.getResult()){
-//                    Log.d("TAG",documentSnapshot.getId() + " => " + documentSnapshot.getData());
-//                }
-            }
-            else{
-                Log.d("TAG","mission failed");
-            }
-        });
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        FragmentProfileBinding binding = FragmentProfileBinding.inflate(inflater, container, false);
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
 
         textName = binding.textName;
         textEmail = binding.textEmail;
         textPhone = binding.textPhone;
+        profilePicture = binding.ProfilePic;
 
-        retrieveInfo();
-
+        retrieveInfo(false);
 
         binding.btnProfileUpdate.setOnClickListener(view -> {
-            retrieveInfo();
+            retrieveInfo(false);
         });
 
         binding.btnEditProfile.setOnClickListener(view -> {
@@ -101,6 +91,9 @@ public class ProfileFragment extends Fragment {
             startActivity(new Intent(getContext(), ReviewsActivity.class));
         });
 
+        binding.btnProfileUpdate.setOnClickListener(view -> {
+            retrieveInfo(true);
+        });
         binding.btnLogout.setOnClickListener(view ->{
             FirebaseAuth.getInstance().signOut();
             Toast.makeText(this.getContext(), "User logged out!", Toast.LENGTH_SHORT).show();
@@ -109,5 +102,59 @@ public class ProfileFragment extends Fragment {
         });
         // Inflate the layout for this fragment
         return binding.getRoot();
+    }
+
+    private void retrieveInfo(Boolean refresh){
+        StorageReference userRef = storageRef.child("media/images/profile_pictures/" + currentUser.getUid());
+        userRef.getMetadata().addOnSuccessListener(storageMetadata -> {
+//            Snackbar.make(binding.getRoot(), "File exists!", Snackbar.LENGTH_SHORT).show();
+            if(refresh){
+                GlideApp.with(this)
+                        .load(userRef)
+                        .signature(new ObjectKey(String.valueOf(System.currentTimeMillis())))
+//                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+//                        .skipMemoryCache(true)
+                        .fitCenter()
+                        .into(profilePicture);
+            }
+            else{
+                GlideApp.with(this)
+                .load(userRef)
+//                .signature(new ObjectKey(String.valueOf(System.currentTimeMillis())))
+                .fitCenter()
+                .into(profilePicture);
+            }
+        }).addOnFailureListener(e -> {
+//            Snackbar.make(binding.getRoot(), "File does not exist!", Snackbar.LENGTH_SHORT).show();
+        });
+        db.collection("users").document(currentUser.getUid()).get().addOnCompleteListener(task -> {
+            user = task.getResult().toObject(User.class);
+            binding.textName.setText(user.getFullName());
+            if(user.getAbout() != ""){
+                binding.tvdesc.setText(user.getAbout());
+            }
+            if(user.getSkills().size() > 0){
+//                binding.
+            }
+        });
+        db.collection("users").whereEqualTo("email", mAuth.getCurrentUser().getEmail()).get().addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                QueryDocumentSnapshot user;
+                if(!task.getResult().iterator().hasNext()){
+                    Toast.makeText(getContext(), "User not found in database!", Toast.LENGTH_SHORT).show();
+                    Log.d("TAG","user not in database");
+                    return;
+                }
+                user = task.getResult().iterator().next();
+
+                textName.setText(user.getString("fullName"));
+                textEmail.setText(user.getString("email"));
+                textPhone.setText(user.getString("phone"));
+
+            }
+            else{
+                Log.d("TAG","mission failed");
+            }
+        });
     }
 }
--- app/src/main/java/com/example/flashgig/models/User.java
+++ app/src/main/java/com/example/flashgig/models/User.java
@@ -1,17 +1,52 @@
 package com.example.flashgig.models;
 
-public class User {
+import java.util.ArrayList;
 
-    public String fullName, email, phone, userId;
+public class User {
+    // Required info
+    public String fullName;
+    public String email;
+    public String phone;
+    public String userId;
+    // Additional info
+    public String about = "";
+    public ArrayList<String> skills = new ArrayList<>();
 
     public User(){
     }
 
+    // Constructor for new Users
     public User(String fullName, String email, String phone, String userId){
         this.fullName = fullName;
         this.email = email;
         this.phone = phone;
         this.userId = userId;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public ArrayList<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(ArrayList<String> skills) {
+        this.skills = skills;
+    }
+
+    // Constructor for existing/editing user profiles
+    public User(String fullName, String email, String phone, String userId, String about, ArrayList<String> skills) {
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.userId = userId;
+        this.about = about;
+        this.skills = skills;
     }
 
     public String getFullName() {
