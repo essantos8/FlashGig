ea8536650894ca2cee9d0ada6f9153633c96adfd
OyoMagpayo <80026713+OyoMagpayo@users.noreply.github.com>
2022-06-13 11:23:47 +0800

    Displayed profile image in Inbox

--- app/src/main/java/com/example/flashgig/activities/InboxChat.java
+++ app/src/main/java/com/example/flashgig/activities/InboxChat.java
@@ -6,23 +6,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
 
 
+import com.bumptech.glide.signature.ObjectKey;
+import com.example.flashgig.GlideApp;
 import com.example.flashgig.R;
 import com.example.flashgig.databinding.ActivityInboxChatBinding;
 import com.example.flashgig.databinding.ActivityMainBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class InboxChat extends AppCompatActivity {
     private ActivityInboxChatBinding binding;
+
+    private FirebaseAuth mAuth;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+    private FirebaseUser currentUser;
 
     
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+        currentUser = mAuth.getCurrentUser();
+        
+        
         binding = ActivityInboxChatBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
+        loadUserDetails();
+    }
+
+    private void loadUserDetails() {
+        StorageReference userRef = storageRef.child("media/images/profile_pictures/" + currentUser.getUid());
+        GlideApp.with(this)
+                .load(userRef)
+                .fitCenter()
+                .into(binding.imageProfile);
+
     }
 
     private void setListeners(){
